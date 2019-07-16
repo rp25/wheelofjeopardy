0c93235e7fd7dcf3ec1a024918d8b952524f048d
Rajan Patel <rp25@users.noreply.github.com>
2019-07-16 14:30:36 -0400

    Update QCASystem.py

--- QCASystem.py
+++ QCASystem.py
@@ -1,24 +1,39 @@
-# -*- coding: utf-8 -*-
 """
 Created on Mon Jul 15 10:16:26 2019
-
 @author: Rajan
 """
 
 import pandas as pd
 import gameDatabase
 
-
+class QCAL():
+    def __init__(self, q, c, a, l):
+        self.question = q
+        self.category = c
+        self.answer = a
+        self.level = l
+        self.used = False
 
 class QCASystem:
+    
     def __init__(self, name):
         self.name = name    
+        self.db = gameDatabase.database('default')
+
+    #add players after db is loaded since it clears players and high scores
+    def loadDefaultQCA(self, path = 'default.p'):
+        self.db.loadDB(path)
         
-    def function():
-        pass
-
-    def loadQuestionsFromDB():
-        '''Pull current QCA from DB'''
-        pass
-
-
+    def addCategory(self, cat):
+         if(cat not in self.db.getAllCategories()):
+             self.db[cat] = []
+         else:
+             print('Category already exist')
+             
+    def addQA(self, cat, QA): #('Q', 'A', False)
+         if(cat in self.db.getAllCategories()):
+             #self.db[cat].append(QA)
+             self.db[cat] = self.db[cat].append(QA)
+         else:
+             print('Category does not exist')
+             
