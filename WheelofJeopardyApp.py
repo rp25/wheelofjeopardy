ab19ac9bf1153c4a238e96a70268c1121ce6662e
Josh Frandsen <joshfrandsen@Joshs-MacBook-Pro.local>
2019-07-16 00:14:11 -0500

    Added two files for the Kivy GUI, initial (ugly) question matrix with a spin button and output label. None of the UI is linked to any other subsystem yet.

---
+++ WheelofJeopardyApp.py
@@ -0,0 +1,16 @@
+import kivy
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class WheelofJeopardy(Widget):
+    def __init__(self, **kwargs):
+        super(WheelofJeopardy, self).__init__(**kwargs)
+
+
+class WheelofJeopardyApp(App):
+    def build(self):
+        return WheelofJeopardy()
+
+if __name__ == "__main__":
+    WheelofJeopardyApp().run()
