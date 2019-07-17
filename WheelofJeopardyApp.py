cdb527bfccd89d2e918ffbc905d647147050bcda
Josh Frandsen <joshfrandsen@Joshs-MacBook-Pro.local>
2019-07-17 00:20:10 -0500

    Now pulling the default quesitons from data base. UI will let you click a question button and the qestion associated with that button will be displayed.

--- WheelofJeopardyApp.py
+++ WheelofJeopardyApp.py
@@ -10,55 +10,55 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty, StringProperty
 
 class QuestionButton(Button):
+    '''TODO: just make this hold the QCA object
+        to make everything much cleaner and less redundant
     '''
-    This class will hold the question and answer button info
-
-    '''
-    def __init__(self, q, a, pts, cat, **kwargs):
+    def __init__(self, q, a, pts, cat, qlabel, **kwargs):
         super(QuestionButton, self).__init__(**kwargs)
-        
+        self.qq = qlabel
         self.question = q
         self.answer = a
         self.points = pts
         self.text = self.points
         self.category = cat
-        self.hasBeenAsked = False
-        self.background_color = (1, 1, 1, 1)
-    
-    def hasBeenAsked(self):
-        return self.hasBeenAsked
+        self.background_color = (1, 1, 1, 0.9)
 
-    def askQuestion(self):
-        self.hasBeenAsked = True
+    # For now just display quesiton if button is clicked
+    def on_press(self):
+        self.qq.text = self.question
 
 class QuestionMatrix(GridLayout):
-    
-    def __init__(self, qcaDict, **kwargs):
+    # TODO: Don't pass around the 'qq' it's ugly
+    def __init__(self, qcaDict, qlabel, **kwargs):
         super(QuestionMatrix, self).__init__(**kwargs)
-        self.cols = 5
+        self.cols = 6
         self.rows = 6
         self.orientation = 'horizontal'
 
         self.questionButtons = []
         self.categoryButtons = []
-        self.buildMatrix(qcaDict)
+        self.buildMatrix(qcaDict, qlabel)
 
-    def buildMatrix(self, qcaDict):
+    def buildMatrix(self, qcaDict, qq):
+        # TODO: The grid fills in left to right
+        # so the categories correspond to rows right now...
+        # fix this
         for category, questions in qcaDict.items():
             button = Button()
             button.text = str(category)
             button.background_color = (1, 0, 0, 1)
             self.categoryButtons.append(button)
-
             self.add_widget(button)
         
         for questions in qcaDict.values():
             for qa in questions:
+                print(qa[1])
                 questionButton = QuestionButton(
-                    str(qa[1]),
                     str(qa[0]),
-                    str(qa[2]),
-                    str(category)
+                    str(qa[1]),
+                    '100',
+                    str(category),
+                    qq
                 )
                 print(f"[i] {questionButton.text}")
                 self.questionButtons.append(questionButton)
@@ -67,49 +67,19 @@ class QuestionMatrix(GridLayout):
 
 class WheelofJeopardy(Widget):
     mainBox = ObjectProperty()
+    question = ObjectProperty()
 
     def __init__(self, **kwargs):
         super(WheelofJeopardy, self).__init__(**kwargs)
-
-        testDict = {
-            'cat1' : [
-                ('a1', 'q1', '100'),
-                ('a2', 'q2', '200'),
-                ('a3', 'q3', '300'),
-                ('a4', 'q4', '400'),
-                ('a5', 'q5', '500'),
-            ],
-            'cat2' : [
-                ('a1', 'q1', '100'),
-                ('a2', 'q2', '200'),
-                ('a3', 'q3', '300'),
-                ('a4', 'q4', '400'),
-                ('a5', 'q5', '500'),
-            ],
-            'cat3' : [
-                ('a1', 'q1', '100'),
-                ('a2', 'q2', '200'),
-                ('a3', 'q3', '300'),
-                ('a4', 'q4', '400'),
-                ('a5', 'q5', '500'),
-            ],
-            'cat4' : [
-                ('a1', 'q1', '100'),
-                ('a2', 'q2', '200'),
-                ('a3', 'q3', '300'),
-                ('a4', 'q4', '400'),
-                ('a5', 'q5', '500'),
-            ],
-            'cat5' : [
-                ('a1', 'q1', '100'),
-                ('a2', 'q2', '200'),
-                ('a3', 'q3', '300'),
-                ('a4', 'q4', '400'),
-                ('a5', 'q5', '500'),
-            ]
-        }
-        qtest = QuestionMatrix(testDict)
-        qtest.size_hint = (0.7, 0.5)
+        
+        # Load quesitons from QCASystem
+        qcaSystem = QCASystem.QCASystem('default')
+        qcaSystem.loadDefaultQCA()
+        qca = qcaSystem.db.getQCA()
+  
+        # Build matrix of questions
+        qtest = QuestionMatrix(qca, self.question)
+        qtest.size_hint = (0.9, 0.5)
         qtest.pos = self.mainBox.center
         self.mainBox.add_widget( qtest)
 
