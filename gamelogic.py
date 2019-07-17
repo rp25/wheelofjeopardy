aad4d6a7c3e4fed641c164090021445ca145dd3a
Rajan Patel <rp25@users.noreply.github.com>
2019-07-17 12:43:47 -0400

    Update gamelogic.py

--- gamelogic.py
+++ gamelogic.py
@@ -5,8 +5,7 @@ import random
 
 # gamelogic object will hold 12 predefined sectors and handle all game related logics associated with each sector. 
 # For the mininum system - read the category data from the QCAsystem (6 sectors)
-sectors = ['category 1', 'category 2', 'category 3', 'category 4', 'category 5', 'category 6', 
-'lose', 'free', 'bankrupt', 'pchoice', 'ochoice', 'double']
+defaultSectors = ['lose', 'free', 'bankrupt', 'pchoice', 'ochoice', 'double']
 
 round = 1
 spinCount = 0
@@ -14,8 +13,9 @@ playerQ = []
     
 # Return a randomized sector when the player spins the wheel 
 
-def getOneSector():
-    random_pick = random.choice(sectors)
+def getOneSector(catSectors):
+    defaultSectors.extend(catSectors)
+    random_pick = random.choice(defaultSectors)
     
     # For the skeletal system - return the random_pick from the list
     # For the minumum system - return the random_pick by calling its sub function 
