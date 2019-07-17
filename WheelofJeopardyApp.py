2e7549474fa86b18f84e89dfc5baf24548818cfd
Josh Frandsen <joshfrandsen@Joshs-MacBook-Pro.local>
2019-07-16 22:04:41 -0500

    changed gamelogic to only be functions not a class. Adjusted the way the question matrix is drawn. Gamelogic spin method is connected to GUI

--- WheelofJeopardyApp.py
+++ WheelofJeopardyApp.py
@@ -1,15 +1,121 @@
-import QCASystem.py
-import gamelogic.py
+import QCASystem
+import gamelogic
 
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout 
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty, StringProperty
+
+class QuestionButton(Button):
+    '''
+    This class will hold the question and answer button info
+
+    '''
+    def __init__(self, q, a, pts, cat, **kwargs):
+        super(QuestionButton, self).__init__(**kwargs)
+        
+        self.question = q
+        self.answer = a
+        self.points = pts
+        self.text = self.points
+        self.category = cat
+        self.hasBeenAsked = False
+        self.background_color = (1, 1, 1, 1)
+    
+    def hasBeenAsked(self):
+        return self.hasBeenAsked
+
+    def askQuestion(self):
+        self.hasBeenAsked = True
+
+class QuestionMatrix(GridLayout):
+    
+    def __init__(self, qcaDict, **kwargs):
+        super(QuestionMatrix, self).__init__(**kwargs)
+        self.cols = 5
+        self.rows = 6
+        self.orientation = 'horizontal'
+
+        self.questionButtons = []
+        self.categoryButtons = []
+        self.buildMatrix(qcaDict)
+
+    def buildMatrix(self, qcaDict):
+        for category, questions in qcaDict.items():
+            button = Button()
+            button.text = str(category)
+            button.background_color = (1, 0, 0, 1)
+            self.categoryButtons.append(button)
+
+            self.add_widget(button)
+        
+        for questions in qcaDict.values():
+            for qa in questions:
+                questionButton = QuestionButton(
+                    str(qa[1]),
+                    str(qa[0]),
+                    str(qa[2]),
+                    str(category)
+                )
+                print(f"[i] {questionButton.text}")
+                self.questionButtons.append(questionButton)
+                self.add_widget(questionButton)
+
 
 class WheelofJeopardy(Widget):
+    mainBox = ObjectProperty()
+
     def __init__(self, **kwargs):
         super(WheelofJeopardy, self).__init__(**kwargs)
 
+        testDict = {
+            'cat1' : [
+                ('a1', 'q1', '100'),
+                ('a2', 'q2', '200'),
+                ('a3', 'q3', '300'),
+                ('a4', 'q4', '400'),
+                ('a5', 'q5', '500'),
+            ],
+            'cat2' : [
+                ('a1', 'q1', '100'),
+                ('a2', 'q2', '200'),
+                ('a3', 'q3', '300'),
+                ('a4', 'q4', '400'),
+                ('a5', 'q5', '500'),
+            ],
+            'cat3' : [
+                ('a1', 'q1', '100'),
+                ('a2', 'q2', '200'),
+                ('a3', 'q3', '300'),
+                ('a4', 'q4', '400'),
+                ('a5', 'q5', '500'),
+            ],
+            'cat4' : [
+                ('a1', 'q1', '100'),
+                ('a2', 'q2', '200'),
+                ('a3', 'q3', '300'),
+                ('a4', 'q4', '400'),
+                ('a5', 'q5', '500'),
+            ],
+            'cat5' : [
+                ('a1', 'q1', '100'),
+                ('a2', 'q2', '200'),
+                ('a3', 'q3', '300'),
+                ('a4', 'q4', '400'),
+                ('a5', 'q5', '500'),
+            ]
+        }
+        qtest = QuestionMatrix(testDict)
+        qtest.size_hint = (0.7, 0.5)
+        qtest.pos = self.mainBox.center
+        self.mainBox.add_widget( qtest)
 
+    def changeText(self, label):
+        label.text = gamelogic.getOneSector()
+        
 class WheelofJeopardyApp(App):
     def build(self):
         return WheelofJeopardy()
--- gamelogic.py
+++ gamelogic.py
@@ -4,51 +4,49 @@ import numpy as np
 import random
 
 # gamelogic object will hold 12 predefined sectors and handle all game related logics associated with each sector. 
-class gamelogic:
+# For the mininum system - read the category data from the QCAsystem (6 sectors)
+sectors = ['category 1', 'category 2', 'category 3', 'category 4', 'category 5', 'category 6', 
+'lose', 'free', 'bankrupt', 'pchoice', 'ochoice', 'double']
+
+round = 1
+spinCount = 0
+playerQ = [] 
     
-    def __init__(self):
-        # For the mininum system - read the category data from the QCAsystem (6 sectors)
-        self.sectors = ['category 1', 'category 2', 'category 3', 'category 4', 'category 5', 'category 6', 
-        'lose', 'free', 'bankrupt', 'pchoice', 'ochoice', 'double']
-        
-        self.round = 1
-        self.spinCount = 0
-        self.playerQ = [] 
-        
-    # Return a randomized sector when the player spins the wheel 
-    def getOneSector(self):
-        random_pick = random.choice(self.sectors)
-        
-        # For the skeletal system - return the random_pick from the list
-        # For the minumum system - return the random_pick by calling its sub function 
-        return random_pick
+# Return a randomized sector when the player spins the wheel 
+
+def getOneSector():
+    random_pick = random.choice(sectors)
     
-    # Return the name of the sector and one category randomly selected   
-    def getCategories(self):
-        # Read the category data from the QCAsystem (6 sectors)
-        # Randomize the category data and select one to return
-        pass
-        
-    # Return the name of the sector and allow the player to use the "FreeTurn" token if exists
-    def loseTurn(self):
-        pass
-        
-    # Return the name of the sector and reward the player with a "FreeTurn" token
-    def freeTurn(self):
-        pass
-        
-    # Return the name of the sector and take away oall of the player's current score (stored in the QCAsystem). Do not allow "Free Turn" token to be used
-    def bankrupt(self):
-        pass
-        
-    # Return the name of the sector and allow the player to choose a category to answer
-    def playerChoice(self):
-        pass
-        
-    # Return the name of the sector and allow the player's opponent to select a category     
-    def opponentChoice(self):
-        pass
-        
-    # Return the name of the sector and double the score stored in the QCAsystem   
-    def doubleScore(self):
-        pass
+    # For the skeletal system - return the random_pick from the list
+    # For the minumum system - return the random_pick by calling its sub function 
+    return random_pick
+
+# Return the name of the sector and one category randomly selected   
+def getCategories():
+    # Read the category data from the QCAsystem (6 sectors)
+    # Randomize the category data and select one to return
+    pass
+    
+# Return the name of the sector and allow the player to use the "FreeTurn" token if exists
+def loseTurn():
+    pass
+    
+# Return the name of the sector and reward the player with a "FreeTurn" token
+def freeTurn():
+    pass
+    
+# Return the name of the sector and take away oall of the player's current score (stored in the QCAsystem). Do not allow "Free Turn" token to be used
+def bankrupt():
+    pass
+    
+# Return the name of the sector and allow the player to choose a category to answer
+def playerChoice():
+    pass
+    
+# Return the name of the sector and allow the player's opponent to select a category     
+def opponentChoice():
+    pass
+    
+# Return the name of the sector and double the score stored in the QCAsystem   
+def doubleScore():
+    pass
