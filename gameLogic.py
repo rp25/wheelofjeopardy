7decee492580d4222ccbeb9fbfa45bf1d3b6c2d7
Rajan Patel <rp25@users.noreply.github.com>
2019-08-05 15:11:25 -0400

    Create gameLogic.py

---
+++ gameLogic.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  5 15:06:03 2019
+
+@author: Rajan
+"""
+
+class gameLogic:
+    
+    def __init__(self):
+        self.defaultSectors = []
+        
+    def getOneSector(self, categories):
+        pass
+        
+    def loseTurn(self, player):
+        pass
+    
+    #give player a turn counter
+    def freeTurn(self, player):
+        pass
+    
+    def bankrupt(self, player):
+        pass
+    
+    def playerChoice(self, player):
+        pass
+    
+    def opponentChoice(self, player, allPlayers):
+        pass
+    
+    def doubleScore(self, player):
+        pass
+    
+    def changeRound(self, roundNum):
+        pass
+    
+    def spinCounter(self):
+        pass
+    
+    def getCurrentPlayer(self, allPlayers):
+        pass
+        
